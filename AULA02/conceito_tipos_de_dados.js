76a8a0d5cb45c37c9398101e5ec7ae2ec4248b9c
claracostaa <mariaclcostabeli@gmail.com>
2026-09-18 11:58:45 -0300

    back-end

--- AULA02/conceito_tipos_de_dados.js
+++ AULA02/conceito_tipos_de_dados.js
@@ -27,3 +27,16 @@ console.log(typeof nomeDois);
 
 let soma = 10 + 5;
 console.log(soma);
+
+// Boolean (Verdadeiro ou falso)
+// Um booblean pode ter apenas dois valores: true (verdadeiro) ou false (falso)
+
+let maiorDeIdade = true;
+let menorDeIdade = false;
+
+console.log(maiorDeIdade); //Exibe true
+console.log(menorDeIdade); //Exibe false
+
+let idade = 16;
+let podeDirigir = idade >= 18;
+console.log(podeDirigir);
