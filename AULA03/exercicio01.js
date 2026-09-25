a5226c2bdb552d6c1f40ef0befab23bf566f39fd
claracostaa <mariaclcostabeli@gmail.com>
2026-09-25 11:26:43 -0300

    exercicio01

--- AULA03/exercicio01.js
+++ AULA03/exercicio01.js
@@ -197,18 +197,18 @@ const conta = {
     curso: "Desenvolvimento de Sistemas",
     turma: "DS 2",
     idade: 16
-};
+}
 
 // Utilize os seguintes dados:
-console.log("Nome do usuário:", conta.usuario);
-console.log("Nome completo:", conta.nome);
-console.log("E-mail:", conta.email);
-console.log("Curso:", conta.curso);
-console.log("Turma:", conta.turma);
-console.log("Idade:", conta.idade);
-console.log("Nível de acesso:", conta.nivel);
-console.log("Senha:", conta.senha);
+console.log("Nome do usuário:", conta.usuario)
+console.log("Nome completo:", conta.nome)
+console.log("E-mail:", conta.email)
+console.log("Curso:", conta.curso)
+console.log("Turma:", conta.turma)
+console.log("Idade:", conta.idade)
+console.log("Nível de acesso:", conta.nivel)
+console.log("Senha:", conta.senha)
 
 // Depois de criar o objeto, utilize as propriedades do objeto para montar e exibir no console as seguintes informações:
-const mensagem = `O usuário ${conta.usuario}, de ${conta.idade} anos, está matriculado no curso de ${conta.curso}, turma ${conta.turma}. Seu nível de acesso é ${conta.nivel} e seu e-mail é ${conta.email}. A senha cadastrada é ${conta.senha}.`;
-console.log(mensagem);
+const mensagem = `O usuário ${conta.usuario}, de ${conta.idade} anos, está matriculado no curso de ${conta.curso}, turma ${conta.turma}. Seu nível de acesso é ${conta.nivel} e seu e-mail é ${conta.email}. A senha cadastrada é ${conta.senha}.`
+console.log(mensagem)
