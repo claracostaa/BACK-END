88d231b3ddeba556920fced7ab8a0a887a01e530
claracostaa <mariaclcostabeli@gmail.com>
2026-09-25 11:43:16 -0300

    atividade02

---
+++ EXERCICIO03/array.js
@@ -0,0 +1,27 @@
+// Exercício 1 — Criando array simples
+// Crie um array chamado cores com três cores e mostre a segunda cor no console.
+let cores = ["Branco", "Rosa", "Azul"];
+console.log(cores[2])
+
+// Exercício 2 — Primeiro e último número
+// Crie um array numeros com cinco números e mostre o primeiro e o último.
+let numeros = ["12", "13", "14", "15", "16"];
+console.log(numeros[0])
+console.log(numeros[4])
+
+// Exercício 3 — Adicionando item ao array
+// Crie um array frutas com duas frutas. Depois adicione uma terceira usando push() e mostre o array.
+let frutas = ["Morango", "Banana"];
+frutas.push("Laranja");
+console.log(frutas);
+
+// Exercício 4 — Removendo o último item
+// Crie um array nomes com quatro nomes e remova o último usando pop().
+let nomes = ["Luiza", "Ana", "Lucas", "Gabriel"];
+nomes.pop(nomes);
+console.log(nomes);
+
+// Exercício 5 — Contando elementos
+// Crie um array times com quatro times e mostre quantos elementos existem nele.
+let times = ["Palmeiras", "Santos", "São Paulo", "Corinthians"];
+console.log(times);
